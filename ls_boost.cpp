31b90fedf55d1d0964913761d91a8f7879ecf44e
Gelya <burangelina@yandex.ru>
2017-05-26 18:22:38 +0300

    main

--- ls_boost.cpp
+++ ls_boost.cpp
@@ -29,46 +29,33 @@ Text_Mode::Text_Mode(const std::string & Mode) : mode(Mode)
 };
 
 Text_Mode::~Text_Mode()
-{
-    
-    std::cout << NORM_MODE << ' ';
+{    
+    std::cout << NORM_MODE;
 };
 
 
 
-void dirwalk(const path & p, vec & arr)
+void dirwalk(const path & dir, vec & arr)
 {
-     
-      if (is_directory(p)) {     
-      
-          std::cout << p << " is a directory containing:\n";       
- 
-          std::copy(directory_iterator(p), directory_iterator(), back_inserter(arr));     
-
-          for (vec::const_iterator it(arr.begin()), it_end(arr.end()); it != it_end; ++it) {             
-          
-              if (!is_directory((*it))) { 
-                  if (it->filename().native().find(".txt") != std::string::npos) {
-                      Text_Mode red(RED_BOLD);
-                      std::cout << it->filename();
-                  } 
-                  else if (it->filename().native().find(".") == std::string::npos) {
-                      Text_Mode red(GREEN_BOLD);
-                      std::cout << it->filename();
-                  }
-                  else {     
-                      std::cout << "   " << it->filename();              
-                  }    
-                  std::cout << file_size((*it)) << '\n';
-              }    
-              else {
-                  Text_Mode blue(BLUE_BOLD);
-                  std::cout << (*it).filename() << '\n';   
-              
-              }  
-          }
-      }   
-
+    std::cout << dir << " is a directory containing:\n";  
+    std::copy(directory_iterator(dir), directory_iterator(), back_inserter(arr));
+    for (const auto& p : arr) {
+        if (!is_directory(p)) { 
+            if (p.filename().native().find(".txt") != std::string::npos) {
+                Text_Mode red(RED_BOLD);
+                std::cout << p.filename() << ' ';
+            } else if (p.filename().native().find(".") == std::string::npos) {
+                Text_Mode red(GREEN_BOLD);
+                std::cout << p.filename() << ' ';
+            } else {     
+                std::cout << "   " << p.filename() << ' ';              
+            }    
+            std::cout << file_size(p) << '\n';
+        } else {
+            Text_Mode blue(BLUE_BOLD);
+            std::cout << p.filename() << '\n';   
+        }  
+    }    
 }
 
 void fsize(const path & p)
@@ -78,36 +65,29 @@ void fsize(const path & p)
     std::cout  << std::endl;
     if (is_directory(p)) {        
         dirwalk(p, arr);
-    }    
-    else {        
+    } else {        
         std::cout << p.filename() << " " << file_size(p) << std::endl;
     }    
     if (arr.empty()) {
         return;
-    }    
-      
-    else {
-        for (int i = 0; i < arr.size(); i++) {
-                     
+    } else {
+        for (int i = 0; i < arr.size(); i++) {                    
             if (is_directory(arr[i]))
                 fsize(arr[i]);
-        }        
-            
-                    
-    }    
-        
-            
+        }                        
+    }             
 }
 
 int main(int argc, char** argv)
 {
+    if (argc < 2) {
+        std::cerr << "usage: " << argv[0] << " file_name" << '\n'; 
+        return -1;
+    }    
     path p(argv[1]);
-    try 
-    {
+    try {
         fsize(p);
-    }   
-    catch (const filesystem_error& ex)
-    {
+    } catch (const filesystem_error& ex) {
         std::cout << ex.what() << '\n';
     } 
     return 0;
